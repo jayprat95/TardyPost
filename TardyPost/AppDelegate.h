4de69722787f3ae3869b1b6343900c303dec68b4
Jayanth Prathipati <jprathipati@Jayanths-MBP.home>
2015-06-12 21:11:21 -0400

    Initial Commit

---
+++ TardyPost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TardyPost
+//
+//  Created by Jayanth Prathipati on 6/12/15.
+//  Copyright (c) 2015 TouchTap Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TardyPost/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  TardyPost
+//
+//  Created by Jayanth Prathipati on 6/12/15.
+//  Copyright (c) 2015 TouchTap Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
